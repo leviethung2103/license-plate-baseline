e3d6f08541d47b8e42015ca70946c5afec0f492c
Hung Le Viet <hunglv@greenglobal.vn>
2020-07-14 14:48:58 +0700

    update main

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import cv2
 import os 
-from license_recog.detector import LP_Detector 
-from license_recog.utils import *
+from license_recognition import models
+from license_recognition.utils.tools import get_cfg
+from license_recognition.utils.tools import preprocess_img
+
+# show available models
+models.show_avai_models()
 
 
 cfg_path = './configs/server_api.yaml'
+
 config = get_cfg(cfg_path)
 
+# init WPOD & OCR model
+wpod_model = models.build_model('wpod',cfg_path)
+ocr_model = models.build_model('ocr',cfg_path)
+
+# prepare data
 img_path = config['input']['image']
 img = cv2.imread(img_path)
-
-
-# init model
-LP_Recognizer = LP_Detector(config)
 
 # preprocessing 
 input_img,resized,origin  = preprocess_img(img)
 
 # predict
-lp_image, is_square = LP_Recognizer.get_plate(input_img,resized,origin)
+lp_image, is_square = wpod_model.get_plate(input_img,resized,origin)
 
 if not os.path.exists("logs/outputs"):
     os.makedirs("logs/outputs")
@@ -26,7 +32,7 @@ if not os.path.exists("logs/outputs"):
 if lp_image is not None:
     print (f"[INFO] Check the result at: logs/outputs")
     cv2.imwrite('logs/outputs/output.png',lp_image)
-    lp_string = LP_Recognizer.get_number(lp_image)
+    lp_string = ocr_model.get_number(lp_image)
     print ("[INFO] License Plate:",lp_string)
 else:
     print (f"[INFO] Didnt recognize license plate")
